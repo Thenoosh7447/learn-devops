9c51f9621bb82e7cbc8fb41ccb99f650366b6389
EC2 Default User <ec2-user@ip-172-31-41-106.ap-south-1.compute.internal>
2026-01-27 13:40:53 +0000

    added create instance

--- day4/create_ec2.sh
+++ day4/create_ec2.sh
@@ -6,13 +6,13 @@ check_cli() {
 		echo "AWS CLI is installed ---" 
 	else
 		echo "AWS CLI isn't installed"
-		installCli
+		install_cli
 	fi
 	
 }
 
 
-installCli() {
+install_cli() {
 	echo "*************** Insalling Amazon CLI............."
 	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 	unzip awscliv2.zip
@@ -21,11 +21,20 @@ installCli() {
 }
 
 
-createInstance() {
-
+create_instance() {
+aws ec2 run-instances \
+       	--image-id ami-0ff5003538b60d5ec \
+       	--count 1 \
+       	--instance-type t3.micro \
+       	--key-name test_mac_ssh \
+	--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="My-Test-Server"}]" \
+       	--security-group-ids sg-0e6f09bd1dc977535 \
+       	--subnet-id subnet-07be38237f07674f2 \
+	--query 'Instances[0].InstanceId' \
+	--output text
 }
 
 
 check_cli
-
+create_instance
 
