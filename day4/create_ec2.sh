11633b4fd4e9c116db8673ed2dd50d1dd09e714d
EC2 Default User <ec2-user@ip-172-31-34-104.ap-south-1.compute.internal>
2026-01-28 13:43:05 +0000

    added extra code

--- day4/create_ec2.sh
+++ day4/create_ec2.sh
@@ -20,21 +20,41 @@ install_cli() {
 	echo "************Installed Amazon CLI*************"
 }
 
-
 create_instance() {
+	local image_id=$1
+	local instance_type=$2
+	local key_name=$3
+	local security_group_ids=$4
+	local subnet_id=$5
+	local instance_name=$6
+	
 aws ec2 run-instances \
-       	--image-id ami-0ff5003538b60d5ec \
-       	--count 1 \
-       	--instance-type t3.micro \
-       	--key-name test_mac_ssh \
-	--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="My-Test-Server"}]" \
-       	--security-group-ids sg-0e6f09bd1dc977535 \
-       	--subnet-id subnet-07be38237f07674f2 \
-	--query 'Instances[0].InstanceId' \
-	--output text
+        --image-id $image_id \
+        --count 1 \
+        --instance-type $instance_type \
+        --key-name $key_name \
+        --security-group-ids $security_group_ids \
+        --subnet-id $subnet_id \
+	--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance_name}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text
 }
 
 
+
+
+main() {
 check_cli
-create_instance
+
+       	amiId="ami-0ff5003538b60d5ec"
+       	iType="t3.micro"
+       	kName="test_mac_ssh"
+       	sgId="sg-0e6f09bd1dc977535"
+       	sId="subnet-07be38237f07674f2"
+	iName="Test-server"
+
+	create_instance "$amiId" "$iType" "$kName" "$sId" "$sgId" "$iName"
+}
+
+main
 
